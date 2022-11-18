51d1b415e054e92929f1db05bcca7a3bd63d9174
JonatSrG <JonatSrG>
2022-11-17 19:21:40 -0600

    Tomar foto y apagar camara

--- public/js/libs/camara-class.js
+++ public/js/libs/camara-class.js
@@ -27,5 +27,35 @@ class Camra {
 
     apagar() {
 
+        this.videoNode.pause();
+        if( this.stream ) {
+
+            this.stream.getTracks()[0].stop();
+
+        }
+    
+    }
+
+    tomarFoto() {
+
+        let canvas = Document.createElement('canvas');
+
+        //dimensiones 
+        canvas.setAttribute('width', 300);
+        canvas.setAttribute('height', 300);
+
+        //obtener contexto canvas
+        let context = canvas.getContext('2d');//simple image
+        
+        context.drawImage( this.videoNode, 0 , 0, canvas.width, canvas.height);
+
+        this.foto = context.canvas.toDataURL();
+
+        //limpieza
+        canvas = null;
+        context = null;
+
+        return this.foto;
+        
     }
 }
