63df36cac1cf0f3b40d3568d1d306105f4aa29fa
JonatSrG <JonatSrG>
2022-11-17 19:07:57 -0600

    Mostrar Camara

--- public/js/app.js
+++ public/js/app.js
@@ -76,6 +76,9 @@ var foto = null;
 // El usuario, contiene el ID del héroe seleccionado
 var usuario;
 
+//init de la camara
+//document.getElementById('player');
+const camara = new Camara( $('#layer')[0] );
 
 
 // ===== Codigo de la aplicación
@@ -509,6 +512,10 @@ btnLocation.on('click', () => {
 btnPhoto.on('click', () => {
 
     console.log('Inicializar camara');
+    contenedorCamara.removeClass('oculto');
+
+    camara.encender();
+
 
 });
 
